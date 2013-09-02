8927b8e6d1fd8e768f085852b0336165c5a93e7e
Michael Patoine <marashu@users.noreply.github.com>
2013-09-02 15:46:11 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace AuxilliumMagi
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (GameCore game = new GameCore())
+            {
+                game.Run();
+            }
+        }
+    }
+}
